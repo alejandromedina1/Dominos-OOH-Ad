7fd5d6953425a34172f40d5b2fdeac86ec215ca7
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2022-08-27 09:21:21 -0500

    Added ngrok but failed to connect to the server

--- public-mupi/mupi-sketch.js
+++ public-mupi/mupi-sketch.js
@@ -1,14 +1,16 @@
-let socket = io("http://localhost:5050", { path: '/real-time' })
+const NGROK = `https://${window.location.hostname}`
+let socket = io(NGROK, { path: '/real-time' })
+console.log('Server IP: ', NGROK)
 let canvas;
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.style('position', 'fixed');
-    canvas.style('top', '0');
-    canvas.style('right', '0');
+    canvas = createCanvas(windowWidth, windowHeight)
+    canvas.style('position', 'fixed')
+    canvas.style('top', '0')
+    canvas.style('right', '0')
 }
 
 function draw() {
-    background(255, 50);
-    fill(0);
-    ellipse(pmouseX, pmouseY, 50, 50);
+    background(255, 50)
+    fill(0)
+    ellipse(pmouseX, pmouseY, 50, 50)
 }
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const { Server } = require('socket.io')
+const PORT = 5050;
+const SERVER_IP = '172.30.70.73' //Use computer's IP
 
 const expressApp = express()
-
 expressApp.use(express.json())
 expressApp.use('/app', express.static('public-mobile'))
 expressApp.use('/mupi', express.static('public-mupi'))
 
 const httpServer = expressApp.listen(5050, () => {
-    console.log(`http//localhost:5050/app`)
+    console.log(`http://${SERVER_IP}:${PORT}/app`)
+    console.log(`http://${SERVER_IP}:${PORT}/mupi`)
 })
+
+// To run on terminal: http ngrok 5050
 
 const io = new Server(httpServer, {path: '/real-time' })
 
@@ -22,6 +26,6 @@ expressApp.get('/mupi', (req, res) => {
 
 io.on('connection', (socket) => { //Listening for webSocket connections
     console.log('Connected!', socket.id)
-});
+})
 
 
